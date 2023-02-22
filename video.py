b613dce4d54c4a414e7c9dd012be4c921ec3d02e
Akhil <akhilpdominic2001@gmail.com>
2023-02-22 22:59:36 +0530

    Successfully modified video.py, code working perfectly 🥳

--- video.py
+++ video.py
@@ -20,50 +20,96 @@ def cal_yawn(shape):
 	distance = dist.euclidean(top_mean,low_mean)
 	return distance
 
-cam = cv2.VideoCapture(0)
+def cal_eye(shape):
+    distance=[]
+    top_left_eye=shape[36:40]
+    bottom_left_eye=shape[41:42]
+    top_left_mean=np.mean(top_left_eye,axis=0)
+    bottom_left_mean=np.mean(bottom_left_eye,axis=0)
+
+    distance.append(dist.euclidean(top_left_mean,bottom_left_mean))
 
 
-#-------Models---------#
+    top_right_eye=shape[43:46]
+    bottom_right_eye=shape[47:48]
+    top_right_mean=np.mean(top_right_eye,axis=0)
+    bottom_right_mean=np.mean(bottom_right_eye,axis=0)
+
+    distance.append(dist.euclidean(top_right_mean,bottom_right_mean))
+
+    left_eye_aspect_ratio=0
+    right_eye_aspect_ratio=0
+
+    A=dist.euclidean(shape[37],shape[41])
+    B=dist.euclidean(shape[38],shape[40])
+    C=dist.euclidean(shape[36],shape[39])
+    left_eye_aspect_ratio=(A+B)/(2*C)
+    distance.append(left_eye_aspect_ratio)
+
+    A=dist.euclidean(shape[43],shape[47])
+    B=dist.euclidean(shape[44],shape[46])
+    C=dist.euclidean(shape[42],shape[45])
+    right_eye_aspect_ratio=(A+B)/(2*C)
+    distance.append(right_eye_aspect_ratio)
+
+    return distance
+
+
+camera = cv2.VideoCapture(0)
 face_model = dlib.get_frontal_face_detector()
 landmark_model = dlib.shape_predictor("/Users/akhilpdominic/Desktop/Projects/mainProject/shape_predictor_68_face_landmarks.dat")
 
-#--------Variables-------#
 yawn_thresh = 35
 ptime = 0
 while True :
-	suc,frame = cam.read()
+	suc,cam = camera.read()
 
 	if not suc :
 		break
-
-
-	#---------FPS------------#	
-	ctime = time.time()
-	fps= int(1/(ctime-ptime))
-	ptime = ctime
-	cv2.putText(frame,f'FPS:{fps}',(frame.shape[1]-120,frame.shape[0]-20),cv2.FONT_HERSHEY_PLAIN,2,(0,200,0),3)
-
-	#------Detecting face------#
-	img_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	
+	img_gray = cv2.cvtColor(cam,cv2.COLOR_BGR2GRAY)
 	faces = face_model(img_gray)
+
 	for face in faces:
-		
 		shapes = landmark_model(img_gray,face)
 		shape = face_utils.shape_to_np(shapes)
+		
+		#Left eye points have been identified from shapes[36:42]
+		left_eye = shape[36:42]
+		#right eye points have been identified from shapes[42:47]
+		right_eye=shape[42:48]
+		mouth = shape[48:59]
+		cv2.drawContours(cam,[left_eye,right_eye,mouth],-1,(205, 0, 0),thickness=2)
+		
 
-		#-------Detecting/Marking the lower and upper lip--------#
-		lip = shape[1:10]
-		lip2=shape[11:17]
-		cv2.drawContours(frame,[lip],-1,(0, 165, 255),thickness=3)
+		
 
-		#-------Calculating the lip distance-----#
-		lip_dist = cal_yawn(shape)
-		# print(lip_dist)
-		if lip_dist > yawn_thresh :
-			cv2.putText(frame, f'User Yawning!',(frame.shape[1]//2 - 170 ,frame.shape[0]//2),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,200),2)
+	lip_dist = cal_yawn(shape)
+	print("\n---- Parameters regarding mouth ----")
+	print("\n Distance between top and bottom lip : "+str(lip_dist))
+	eye_dist=cal_eye(shape)
+	print("\n---- Parameters regarding both eyes ----\n")
+	print("Distance between top and bottom left eye lids : "+str(eye_dist[0]))
+	print("Distance between top and bottom right eye lids : "+str(eye_dist[1]))
+	print("EAR left eye : "+str(eye_dist[2]))
+	print("EAR right eye : "+str(eye_dist[3]))
+
+	earThreshold=(eye_dist[2]+eye_dist[3])/2
+
+	# From data acquired from a research article 
+	if(earThreshold)<0.3:
+		print("Eyes closed")
+	else:
+		print("Eyes OPEN")
+
+	print("\n")
+		
+	if lip_dist > yawn_thresh :
+			#cv2.putText(cam, f'User Yawning!',(cam.shape[1]//2 - 170 ,cam.shape[0]//2),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,200),2)
+			print("Person yawning")
 
 
-	cv2.imshow('Webcam' , frame)
+	cv2.imshow('Webcam' , cam)
 	if cv2.waitKey(1) & 0xFF == ord('q') :
 		break
 
